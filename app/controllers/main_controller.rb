79eef98484c50e685ecbef91e05450343bfe5993
Josh Haywood <josh.haywood@validic.com>
2016-04-02 16:11:58 -0400

    use responders gem to remove respond_to do in controller methods

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,11 +1,10 @@
 class MainController < ApplicationController
+  respond_to :js
+
   def index
   end
 
   def show
     @forecast = Wunderground::Resources.forecast(lat: params[:locationLat], long: params[:locationLong])
-    respond_to do |format|
-      format.js
-    end
   end
 end
