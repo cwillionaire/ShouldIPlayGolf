6ee0c0ada831002303ee9eefee0657dfe5a76a30
Josh Haywood <josh.haywood@validic.com>
2016-04-05 17:20:21 -0400

    update controller specs

--- spec/controllers/main_controller_spec.rb
+++ spec/controllers/main_controller_spec.rb
@@ -19,20 +19,20 @@ describe MainController, type: :controller do
 
     it "responds successfully with an HTTP 200 status code" do
       expect(Wunderground::Resources).to receive(:method_missing).and_return(fixture)
-      xhr :get, :show, zip_code: '10001', format: :js
+      xhr :get, :show, locationLat: 35.555, locationLong: 70.000, format: :js
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
 
     it "renders the show template" do
       expect(Wunderground::Resources).to receive(:method_missing).and_return(fixture)
-      xhr :get, :show, zip_code: '10001', format: :js
+      xhr :get, :show, locationLat: 35.555, locationLong: 70.000, format: :js
       expect(response).to render_template('show')
     end
 
     it "assigns the forecast object" do
       expect(Wunderground::Resources).to receive(:method_missing).and_return(fixture)
-      xhr :get, :show, zip_code: '10001', format: :js
+      xhr :get, :show, locationLat: 35.555, locationLong: 70.000, format: :js
       expect(assigns(:forecast)).to eq fixture
     end
   end
